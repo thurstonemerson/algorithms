9d9ce2e7f6423f7cd437ad8d7df71109c791a672
thurstonemerson <jmt.emerson@gmail.com>
2016-05-19 10:54:50 +1200

    feat: assert that fizzbuzz returns correct values, use stringbuilder instead of concatenating

--- src/com/epi/warmup/FizzBuzz.java
+++ src/com/epi/warmup/FizzBuzz.java
@@ -1,12 +1,11 @@
 package com.epi.warmup;
 
-import com.epi.AddingCredits.ClientsCreditsInfo;
 
 /**
  * The programming exercises in EPI are representative of what you will be asked
  * in an interview. We do not provide textbook-style review questions.
  * 
- * In this document, we present a small set of programming exercises suitable
+ * We present a small set of programming exercises suitable
  * for someone who needs to review basics. (You are unlikely to be asked these
  * questions - their purpose is to bootstrap reading EPI.)
  * 
@@ -27,39 +26,42 @@ public class FizzBuzz {
 //	all the integers from 1 to N, replacing numbers divisible by 3 with “fizz”, numbers divisible by
 //	5 with “buzz”, and numbers divisible by both with “fizz buzz”.
 	
-	 public static void printFizzBuzz(int N){
+	 public static String getFizzBuzz(int N){
+		 
+		 StringBuilder buf= new StringBuilder();
 		 
 		 for (int x=1; x<=N; x++){
 			 if (x%3 == 0 && x%5 == 0){
-				 System.out.print(" fizz buzz");
+				 buf.append("fizz buzz");
 			 } else if (x%3 == 0){
-				 System.out.print(" fizz");
+				 buf.append("fizz");
 			 } else if (x%5 == 0){
-				 System.out.print(" buzz");
+				 buf.append("buzz");
 			 } else {
-				 System.out.print(" " + x);
+				 buf.append(x);
 			 }
 			 
 			 if (x < N){
-				 System.out.print(", ");
-			 } else {
-				 System.out.println();
-			 }
+				 buf.append(", ");
+			 } 
 		 }
+
+		 System.out.println(buf.toString());
+		 return buf.toString();
 	 }
 	
 	 public static void main(String[] args) {	
 		 	//n is 2 prints 1, 2
-		 	FizzBuzz.printFizzBuzz(2);
+		 	assert("1, 2".equals(FizzBuzz.getFizzBuzz(2).trim()));
 		 	
 		 	//n is 3 prints 1, 2, fizz
-		 	FizzBuzz.printFizzBuzz(3);
+		 	assert("1, 2, fizz".equals(FizzBuzz.getFizzBuzz(3).trim()));
 		 	
 		 	//n is 5 prints 1, 2, fizz, 4, buzz
-		 	FizzBuzz.printFizzBuzz(5);
+		 	assert("1, 2, fizz, 4, buzz".equals(FizzBuzz.getFizzBuzz(5).trim()));
 		 	
 		 	//n is 15 prints 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizz buzz 
-		 	FizzBuzz.printFizzBuzz(15);
+		 	assert("1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizz buzz".equals(FizzBuzz.getFizzBuzz(15).trim()));
 		}
 	
 }
