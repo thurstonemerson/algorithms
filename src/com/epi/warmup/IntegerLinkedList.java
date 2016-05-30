e1d3c646d9716c54e98905ff9c5692144ecd22aa
thurstonemerson <jmt.emerson@gmail.com>
2016-05-30 13:35:54 +1200

    can add items in the middle of doubly linked list

--- src/com/epi/warmup/IntegerLinkedList.java
+++ src/com/epi/warmup/IntegerLinkedList.java
@@ -57,8 +57,43 @@ public class IntegerLinkedList {
 		tail = newNode;
 	}
 
-	public void addAfter(int data) {
+	public boolean addAfter(int key, int data) {
 
+		//find key
+		Node current = head;
+		Node keyNode = null;
+		
+		while(current != null){
+			if (current.data == key){
+				keyNode = current;
+			}
+			current = current.next;
+		}
+		
+		//if key node is null, then key wasn't found
+		if (keyNode == null){
+			return false;
+		}
+		
+		//otherwise insert the key after the key node
+		addAfter(keyNode, data);
+		return true;
+	}
+	
+	public void addAfter(Node key, int data) {
+		Node newNode = new Node(data);
+		
+		newNode.next = key.next;
+		
+		if (key != tail){
+			newNode.next.previous = newNode;
+			key.next = newNode;
+			newNode.previous = key;
+		} else {//if key was the tail, point the tail to the new node
+			key.next = newNode;
+			newNode.previous = key;
+			tail = newNode;
+		}
 	}
 
 	public String printFromHead() {
@@ -124,6 +159,26 @@ public class IntegerLinkedList {
 		// check a list of items are inserted at the beginning
 		assert (list.printFromHead().equals("123"));
 		assert (list.printFromTail().equals("321"));
+		
+		//check can add an item in the middle
+		list = new IntegerLinkedList();
+		list.addFirst(1);
+		list.addLast(3);
+
+		assert (list.printFromHead().equals("13"));
+		assert (list.printFromTail().equals("31"));
+		
+		list.addAfter(1, 2);
+		assert (list.printFromHead().equals("123"));
+		assert (list.printFromTail().equals("321"));
+		
+		//check that returns false if key not found
+		assert (list.addAfter(6, 2) == false);
+		
+		//check that tail is correctly pointing to the end after insertion at end
+		list.addAfter(3, 4);
+		assert (list.printFromHead().equals("1234"));
+		assert (list.printFromTail().equals("4321"));
 
 	}
 }
