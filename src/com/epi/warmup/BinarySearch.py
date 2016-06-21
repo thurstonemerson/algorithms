5654db12eae52d40a9605b22cd6915866cc9aa11
thurstonemerson <jmt.emerson@gmail.com>
2016-06-21 16:42:18 +1200

    binary search python

---
+++ src/com/epi/warmup/BinarySearch.py
@@ -0,0 +1,35 @@
+'''
+The programming exercises in EPI are representative of what you will be asked
+in an interview. We do not provide textbook-style review questions.
+  
+We present a small set of programming exercises suitable
+for someone who needs to review basics. (You are unlikely to be asked these
+questions - their purpose is to bootstrap reading EPI.)
+
+When you write your solutions to these exercises, please code to the
+following constraints:
+ 
+- Do not use library calls. 
+- Do not try to design clever solutions. (This means it is fine to use brute-force approaches.) 
+- Assume valid inputs, and ignore internal errors like overflow, IO exceptions, etc. 
+- Write unit tests for your programs. 
+- Analyze your programs for time and space complexity.
+ 
+@author: Jessica
+'''
+
+''' Searching: Write a nonrecursive program to perform binary search over a sorted array. '''
+def search(a, key):
+    
+    left = 0
+    right = len(a)-1
+    
+    #while (left <= right):
+    
+    return -1
+
+if __name__ == "__main__":
+    a = [0, 1, 4, 5, 8, 10]
+    
+    search(a, 4)
+    
