ee8dc302b79bc2972a9be0643095b6ce93e190d5
thurstonemerson <jmt.emerson@gmail.com>
2016-06-22 11:42:37 +1200

    hashtable java

---
+++ src/com/epi/warmup/HashTable.java
@@ -0,0 +1,91 @@
+package com.epi.warmup;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import com.epi.warmup.HashTable.ArrayObject;
+
+/**
+ * The programming exercises in EPI are representative of what you will be asked
+ * in an interview. We do not provide textbook-style review questions.
+ * 
+ * We present a small set of programming exercises suitable for someone who
+ * needs to review basics. (You are unlikely to be asked these questions - their
+ * purpose is to bootstrap reading EPI.)
+ * 
+ * When you write your solutions to these exercises, please code to the
+ * following constraints:
+ * 
+ * - Do not use library calls. - Do not try to design clever solutions. (This
+ * means it is fine to use brute-force approaches.) - Assume valid inputs, and
+ * ignore internal errors like overflow, IO exceptions, etc. - Write unit tests
+ * for your programs. - Analyze your programs for time and space complexity.
+ * 
+ * @author Jessica
+ * 
+ */
+
+public class HashTable {
+	// Hash tables: Write a program that finds the most common object in an
+	// array of objects. The
+	// objects consists of pairs of strings. Treat strings as being the same if
+	// they are equal when converted to lower case.
+	
+	public static class ArrayObject{
+		public String str1 = null;
+		public String str2 = null;
+		
+		public ArrayObject(String str1, String str2){
+			this.str1 = str1;
+			this.str2 = str2;
+		}
+		
+		//return true if both the string objects are not null, and equal each other (ignoring case)
+		public boolean equals(ArrayObject obj){
+			if (obj != null && obj.str1 != null && this.str1 != null && obj.str1.equalsIgnoreCase(this.str1)){
+				if (obj.str2 != null && this.str2 != null && obj.str2.equalsIgnoreCase(this.str2)){
+					return true;
+				}
+			}
+			
+			return false;
+		}
+	}
+	
+	public static void findMostCommonObjects(ArrayObject[] a){
+		
+		Hashtable<ArrayObject, Integer> objectCount = new Hashtable<ArrayObject, Integer>();
+		
+		for (ArrayObject obj : a){
+			Integer count = objectCount.get(obj);
+			if (count != null){
+				count++;
+			} else {
+				count = 1;
+			}
+			objectCount.put(obj, count);
+		}
+		
+		
+		for (ArrayObject obj : objectCount.keySet()){
+			System.out.println("For object " + obj.str1 + " " + obj.str2 + " the count is " + objectCount.get(obj) );
+		}
+		
+	}
+	
+
+	public static void main(String args[]) {
+		
+		ArrayObject obj1 = new HashTable.ArrayObject("hello", "ick");
+		ArrayObject obj2 = new HashTable.ArrayObject("hello", "ick");
+		ArrayObject obj3 = new HashTable.ArrayObject("", "ick");
+		ArrayObject obj4 = new HashTable.ArrayObject("ff", "asdf");
+		ArrayObject obj5 = new HashTable.ArrayObject("ff", "asdf");
+		ArrayObject obj6 = new HashTable.ArrayObject(null, null);
+
+		ArrayObject[] a = new ArrayObject[]{obj1, obj2, obj3, obj4, obj5, obj6};
+		findMostCommonObjects(a);
+		
+	}
+
+}
