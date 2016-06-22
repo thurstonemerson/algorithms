0dfe5c5abef447b8599600d3c16b82f2d9a93f11
thurstonemerson <jmt.emerson@gmail.com>
2016-06-23 09:38:57 +1200

    Binary tree search python

---
+++ src/com/epi/warmup/BinarySearchTree.py
@@ -0,0 +1,51 @@
+'''
+The programming exercises in EPI are representative of what you will be asked
+in an interview. We do not provide textbook-style review questions.
+  
+We present a small set of programming exercises suitable
+for someone who needs to review basics. (You are unlikely to be asked these
+questions - their purpose is to bootstrap reading EPI.)
+
+When you write your solutions to these exercises, please code to the
+following constraints:
+ 
+- Do not use library calls. 
+- Do not try to design clever solutions. (This means it is fine to use brute-force approaches.) 
+- Assume valid inputs, and ignore internal errors like overflow, IO exceptions, etc. 
+- Write unit tests for your programs. 
+- Analyze your programs for time and space complexity.
+ 
+@author: Jessica
+'''
+
+'''   BSTs: Write a program that searches a BST on integer keys for a given value. (You will need to
+           implement a class suitable for representing BSTs, but do not need to implement
+           add, lookup, delete, etc. methods.)'''
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+        
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        
+    def search(self, current_node, key):
+        #case where key is found or current node is null (no key found in tree)
+        if current_node is None or current_node.data == key:
+            return current_node 
+        
+        #search further in the left or right branches
+        if current_node.data < key:
+            return search(current_node.right, key)
+        else:
+            return search(current_node.left, key)
+        
+        
+        
+        
+
+
+    
