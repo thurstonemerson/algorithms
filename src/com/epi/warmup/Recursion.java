0de677a5db31cbfbb53718ab2ccab0e93d9d4beb
thurstonemerson <jmt.emerson@gmail.com>
2016-06-24 21:48:31 +1200

    Recursion template

---
+++ src/com/epi/warmup/Recursion.java
@@ -0,0 +1,26 @@
+package com.epi.warmup;
+
+/**
+ * The programming exercises in EPI are representative of what you will be asked
+ * in an interview. We do not provide textbook-style review questions.
+ * 
+ * We present a small set of programming exercises suitable for someone who
+ * needs to review basics. (You are unlikely to be asked these questions - their
+ * purpose is to bootstrap reading EPI.)
+ * 
+ * When you write your solutions to these exercises, please code to the
+ * following constraints:
+ * 
+ * - Do not use library calls. - Do not try to design clever solutions. (This
+ * means it is fine to use brute-force approaches.) - Assume valid inputs, and
+ * ignore internal errors like overflow, IO exceptions, etc. - Write unit tests
+ * for your programs. - Analyze your programs for time and space complexity.
+ * 
+ * @author Jessica
+ * 
+ */
+
+public class Recursion {
+//	Write a recursive program that takes as input positive integers x and N, and returns x to the power N.
+//	You should use O(log N) multiplications.
+}
