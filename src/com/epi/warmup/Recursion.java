4d4106e6a1cf11fb1123d5fdd1c3787b16d15bc0
thurstonemerson <jmt.emerson@gmail.com>
2016-06-27 12:22:26 +1200

    Recursively compute power function java

--- src/com/epi/warmup/Recursion.java
+++ src/com/epi/warmup/Recursion.java
@@ -21,6 +21,27 @@ package com.epi.warmup;
  */
 
 public class Recursion {
-//	Write a recursive program that takes as input positive integers x and N, and returns x to the power N.
-//	You should use O(log N) multiplications.
+	// Write a recursive program that takes as input positive integers x and N,
+	// and returns x to the power N.
+	// You should use O(log N) multiplications.
+
+	public int power(int x, int n) {
+		if (n == 0)
+			return 1;
+
+		//if even, recursively compute power of n/2 until we are able to multiply
+		//the total together
+		if (n % 2 == 0) {
+			int total = power(x, n / 2);
+			return total * total;
+		}
+		
+		//if odd, subtract one from n to make it even and recursively call power function
+		return power(x, n-1) * x;
+	}
+
+	public void main(String args[]) {
+
+	}
+
 }
