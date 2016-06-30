6e64761ecf43737fa5ad7bde35e0e0edbb739082
thurstonemerson <jmt.emerson@gmail.com>
2016-06-30 14:06:58 +1200

    Dynamic programming template java

---
+++ src/com/epi/warmup/DynamicProgramming.java
@@ -0,0 +1,16 @@
+package com.epi.warmup;
+
+public class DynamicProgramming {
+//	Dynamic Programming: Write a program that takes a positive integer N, and returns the number
+//	of binary strings of length N such that there are no consecutive 1s. For example, if N = 3, the result
+//	is 5, corresponding to the strings 000, 001, 010, 100, 101.
+	
+	public static int noConsectiveOnes(int n){
+	
+		return 0;
+	}
+	
+	public static void main(String args[]){
+		
+	}
+}
