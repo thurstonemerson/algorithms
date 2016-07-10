06210052e4cf2f1c21d2ca09b4edfb630d458772
thurstonemerson <jmt.emerson@gmail.com>
2016-07-11 11:40:34 +1200

    Compute number of binary strings with an array

--- src/com/epi/warmup/DynamicProgramming.java
+++ src/com/epi/warmup/DynamicProgramming.java
@@ -7,10 +7,26 @@ public class DynamicProgramming {
 	
 	public static int noConsectiveOnes(int n){
 	
-		return 0;
+		int a[] = new int[n+1];
+		int b[] = new int[n+1];
+		a[1]=1;
+		b[1]=1;
+		
+		for (int x=2; x<=n; x++){
+			a[x] = a[x-1] + b[x-1];
+			b[x] = a[x-1];
+		}
+		
+		System.out.println("Number of strings ending in zero: " + a[n]);
+		System.out.println("Number of strings ending in one: " + b[n]);
+		return a[n] + b[n];
 	}
 	
 	public static void main(String args[]){
+		
+		assert(noConsectiveOnes(1)==2);
+		assert(noConsectiveOnes(3)==5);
+		assert(noConsectiveOnes(4)==8);
 		
 	}
 }
