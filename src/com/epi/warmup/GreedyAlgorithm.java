137b4dadc92d1020a79d8f61d48c416a99023a7a
thurstonemerson <jmt.emerson@gmail.com>
2016-07-14 21:48:47 +1200

    Template for greedy algorithm

---
+++ src/com/epi/warmup/GreedyAlgorithm.java
@@ -0,0 +1,12 @@
+package com.epi.warmup;
+
+
+public class GreedyAlgorithm {
+//	Greedy Algorithms and Invariants: Write a program that takes an input a positive integer
+//	N, and returns the minimum number of coins in the US coinage system to create N cents. For example, if
+//	N = 37, the answer is 4, corresponding to a quarter, a dime, and two pennies.
+	public static void main(String args[]){
+		
+	}
+	
+}
