8c187879ae239fb0915b30962ee09148938cb9cc
thurstonemerson <jmt.emerson@gmail.com>
2016-07-15 21:27:42 +1200

    Greedy algorithm template python

---
+++ src/com/epi/warmup/GreedyAlgorithm.py
@@ -0,0 +1,25 @@
+'''
+The programming exercises in EPI are representative of what you will be asked
+in an interview. We do not provide textbook-style review questions.
+  
+We present a small set of programming exercises suitable
+for someone who needs to review basics. (You are unlikely to be asked these
+questions - their purpose is to bootstrap reading EPI.)
+
+When you write your solutions to these exercises, please code to the
+following constraints:
+ 
+- Do not use library calls. 
+- Do not try to design clever solutions. (This means it is fine to use brute-force approaches.) 
+- Assume valid inputs, and ignore internal errors like overflow, IO exceptions, etc. 
+- Write unit tests for your programs. 
+- Analyze your programs for time and space complexity.
+ 
+@author: Jessica
+'''
+
+''' 
+   Greedy Algorithms and Invariants: Write a program that takes an input a positive integer
+   N, and returns the minimum number of coins in the US coinage system to create N cents. For example, if
+   N = 37, the answer is 4, corresponding to a quarter, a dime, and two pennies.'''
+
