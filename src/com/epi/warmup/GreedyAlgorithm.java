3ab72ac105152beed2e09ac9a18bcc548e68c3d6
thurstonemerson <jmt.emerson@gmail.com>
2016-07-18 12:25:38 +1200

    Pseudocode for greedy algorithm coin change problem

--- src/com/epi/warmup/GreedyAlgorithm.java
+++ src/com/epi/warmup/GreedyAlgorithm.java
@@ -5,6 +5,22 @@ public class GreedyAlgorithm {
 //	Greedy Algorithms and Invariants: Write a program that takes an input a positive integer
 //	N, and returns the minimum number of coins in the US coinage system to create N cents. For example, if
 //	N = 37, the answer is 4, corresponding to a quarter, a dime, and two pennies.
+// coins = {1, 5, 10, 25, 50, 100}
+
+	//What to do if there is no solution?
+	//What are the time complexities of the following loops?
+	// Greedy solution:
+	//ID 1: - While N > 0
+	//      - x = largest coin that <= N
+	//      - counter++
+	//      - N = N - x
+	
+	//       (or)
+	//      - counter = counter + (n/x)
+	//      - N = N mod x
+	//
+	
+	
 	public static void main(String args[]){
 		
 	}
