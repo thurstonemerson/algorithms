1288e2f199b7a6c2c2340cdeb6f78718d84bf360
thurstonemerson <jmt.emerson@gmail.com>
2016-07-19 10:30:44 +1200

    Greedy version of coin change problem

--- src/com/epi/warmup/GreedyAlgorithm.java
+++ src/com/epi/warmup/GreedyAlgorithm.java
@@ -12,16 +12,53 @@ public class GreedyAlgorithm {
 	// Greedy solution:
 	//ID 1: - While N > 0
 	//      - x = largest coin that <= N
+	//      - if no x return -1
 	//      - counter++
 	//      - N = N - x
 	
 	//       (or)
 	//      - counter = counter + (n/x)
 	//      - N = N mod x
-	//
+	
+	private static final int[] US_COINS = {1, 5, 10, 25, 50, 100};
+	
+	public static int minNoOfCoins_greedy(int n){
+		
+		int counter = 0;
+		
+		while (n > 0){
+			
+			int x = getLargestCoin(n);
+			
+			if (x == -1){ //no solution found
+				return -1;
+			}
+			
+			counter++;
+			n = n - x;
+		}
+		
+		
+		return counter;
+	}
+	
+	private static int getLargestCoin(int value){
+		int x = -1;
+		
+		//get largest coin less than or equal to N
+		for (int coin : US_COINS){
+			if (coin <= value){
+				x = coin;
+			}
+		}
+		
+		return x;
+	}
 	
 	
 	public static void main(String args[]){
+		
+		
 		
 	}
 	
