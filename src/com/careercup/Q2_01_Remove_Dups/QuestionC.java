b3d5b03464f225d3d0a24f66cfb0c4bbc046ae85
thurstonemerson <jmt.emerson@gmail.com>
2016-07-25 10:56:02 +1200

    Changed imports

--- src/com/careercup/Q2_01_Remove_Dups/QuestionC.java
+++ src/com/careercup/Q2_01_Remove_Dups/QuestionC.java
@@ -1,6 +1,6 @@
-package Q2_01_Remove_Dups;
+package com.careercup.Q2_01_Remove_Dups;
 
-import CtCILibrary.LinkedListNode;
+import com.careercup.CtCILibrary.LinkedListNode;
 
 public class QuestionC {
 	public static void deleteDups(LinkedListNode head) {
@@ -26,7 +26,7 @@ public class QuestionC {
 			/* If runner == current, then we didn't find any duplicate 
 			 * elements in the previous for loop.  We then need to 
 			 * increment current.  
-			 * If runner != current, then we must have hit the �break� 
+			 * If runner != current, then we must have hit the ���break��� 
 			 * condition, in which case we found a dup and current has
 			 * already been incremented.*/
 			if (runner == current) {
