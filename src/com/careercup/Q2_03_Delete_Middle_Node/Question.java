2d3375d0122e3f8e23af56346f304447656136c9
thurstonemerson <jmt.emerson@gmail.com>
2016-07-30 00:54:22 +0000

    Question algorithm for delete middle node

---
+++ src/com/careercup/Q2_03_Delete_Middle_Node/Question.java
@@ -0,0 +1,25 @@
+package com.careercup.Q2_03_Delete_Middle_Node;
+
+import com.careercup.CtCILibrary.AssortedMethods;
+import com.careercup.CtCILibrary.LinkedListNode;
+
+public class Question {
+
+	public static boolean deleteNode(LinkedListNode n) {
+		if (n == null || n.next == null) {
+			return false; // Failure
+		} 
+		LinkedListNode next = n.next; 
+		n.data = next.data; 
+		n.next = next.next; 
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		LinkedListNode head = AssortedMethods.randomLinkedList(10, 0, 10);
+		System.out.println(head.printForward());
+		deleteNode(head.next.next.next.next); // delete node 4
+		System.out.println(head.printForward());
+	}
+
+}
