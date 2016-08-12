0d02c7bcf533c0a2125896b19066f9553b0c5b88
thurstonemerson <jmt.emerson@gmail.com>
2016-08-12 19:50:55 +1200

    Depth first search of graph

--- src/com/epi/warmup/Graph.java
+++ src/com/epi/warmup/Graph.java
@@ -11,14 +11,29 @@ public class Graph {
 	
 	public static class Node {
 		public int data;
-		public Node[] children;
+		public boolean visited = false;
+		public Node[] children = new Node[]{};
 		public Node(int data){
 			this.data = data;
 		}
 	}
 	
+	//depth first search is preorder search, visit node first then deep traverse all branches
 	public static void depthFirstSearch(Node node){
 		
+		visit(node);
+		
+		for (Node child : node.children){
+			if (child.visited == false){
+				depthFirstSearch(child);
+			}
+		}
+		
+	}
+	
+	public static void visit(Node node){
+		System.out.println(node.data);
+		node.visited = true;
 	}
 	
 	public static void main(String args[]){
@@ -27,10 +42,11 @@ public class Graph {
 		Node one = new Node(1);
 		Node four = new Node(4);
 		Node five = new Node(5);
-		Node three = new Node(5);
-		Node two = new Node(5);
+		Node three = new Node(3);
+		Node two = new Node(2);
 		root.children = new Node[]{one, four, five};
-		one.children = new Node[]{two, three, four};
+		one.children = new Node[]{three, four};
+		two.children = new Node[]{one};
 		three.children = new Node[]{two, four};
 		depthFirstSearch(root);
 	}
