b29781a37f6c97aa5859050e0dbd4b78f6f5f66b
thurstonemerson <jmt.emerson@gmail.com>
2016-08-22 13:33:38 +1200

    change package name

---
+++ src/com/com/careercup/Q7_02_Call_Center/Respondent.java
@@ -0,0 +1,8 @@
+package com.com.careercup.Q7_02_Call_Center;
+
+class Respondent extends Employee {
+    public Respondent(CallHandler callHandler) {
+    	super(callHandler);
+    	rank = Rank.Responder;
+    }
+}
