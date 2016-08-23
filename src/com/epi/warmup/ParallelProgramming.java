a33ab374af5f82ced1df866af57067eab5474ee4
thurstonemerson <jmt.emerson@gmail.com>
2016-08-23 20:10:02 +1200

    Parallel programming template

--- src/com/epi/warmup/ParallelProgramming.java
+++ src/com/epi/warmup/ParallelProgramming.java
@@ -1,5 +1,7 @@
 package com.epi.warmup;
 
+//Parallel Computing: Write a program which uses two threads to print the numbers from
+//1 to 100 in order. One thread can only print odd numbers, the other can only print even numbers.
 public class ParallelProgramming {
 
 }
