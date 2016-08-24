0668e6163312a8c1f95a3e062dc9d08ee7c0eaa2
thurstonemerson <jmt.emerson@gmail.com>
2016-08-24 12:33:34 +1200

    Parallel programming print 1 to 10 without a lock

--- src/com/epi/warmup/ParallelProgramming.java
+++ src/com/epi/warmup/ParallelProgramming.java
@@ -3,5 +3,27 @@ package com.epi.warmup;
 //Parallel Computing: Write a program which uses two threads to print the numbers from
 //1 to 100 in order. One thread can only print odd numbers, the other can only print even numbers.
 public class ParallelProgramming {
+	
+	private Object lock = new Object();
 
+	public static void main(String args[]) {
+
+		Thread printEven = new Thread(new Runnable(){
+			public void run(){
+				for (int x = 2; x <= 10; x+=2){
+					System.out.println(x);
+				}
+		}});
+		Thread printOdd = new Thread(new Runnable(){
+			public void run(){
+				for (int x = 1; x <= 9; x+=2){
+					System.out.println(x);
+				}
+				
+		}});
+		
+		printEven.start();
+		printOdd.start();
+
+	}
 }
