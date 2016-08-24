412bdbd3e3d3b7133ff221ab1139b4ae80dd336b
thurstonemerson <jmt.emerson@gmail.com>
2016-08-24 13:20:31 +1200

    Parallel programming print 1 to 10 without deadlock possibility

--- src/com/epi/warmup/ParallelProgramming.java
+++ src/com/epi/warmup/ParallelProgramming.java
@@ -3,42 +3,44 @@ package com.epi.warmup;
 //Parallel Computing: Write a program which uses two threads to print the numbers from
 //1 to 100 in order. One thread can only print odd numbers, the other can only print even numbers.
 public class ParallelProgramming {
-	
+
 	private static Object lock = new Object();
+	private static int current = 1;
+	private static int MAX = 10;
 
 	public static void main(String args[]) {
 
-		Thread printEven = new Thread(new Runnable(){
-			public void run(){
-				for (int x = 2; x <= 10; x+=2){
-					synchronized(lock){
-						try {
-							lock.notifyAll();
-							lock.wait();
-							System.out.println(x);
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+		Thread printEven = new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					synchronized (lock) {
+						if (current > MAX) {
+							return;
+						}
+						if (current % 2 == 0) {
+							System.out.println(current);
+							current++;
 						}
 					}
 				}
-		}});
-		Thread printOdd = new Thread(new Runnable(){
-			public void run(){
-				for (int x = 1; x <= 9; x+=2){
-					synchronized(lock){
-						try {
-							lock.wait();
-							lock.notifyAll();
-							System.out.println(x);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+			}
+		});
+		Thread printOdd = new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					synchronized (lock) {
+						if (current > MAX) {
+							return;
+						}
+						if (current % 2 != 0) {
+							System.out.println(current);
+							current++;
 						}
 					}
 				}
-				
-		}});
+
+			}
+		});
 
 		printOdd.start();
 		printEven.start();
